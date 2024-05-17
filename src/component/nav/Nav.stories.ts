38054ee5772b4028f9014f9f5a68a0a0d6a621cc
alifprihantoro <alifprihantoro@gmail.com>
2024-05-17 21:35:58 +0700

    feat(ui): add navigation (#3)

---
+++ src/component/nav/Nav.stories.ts
@@ -0,0 +1,11 @@
+import Nav from './Nav'
+
+export default {
+  title: 'Components/Nav',
+  render: () => {
+    return `<div class="bg-base-100">
+      ${Nav()}
+    </div>`
+  },
+}
+export const Navigation = {}
---
+++ src/component/nav/listHtml.ts
@@ -0,0 +1,14 @@
+import LIST from '../../configs/nav'
+
+export default LIST.map(({ icon, link, title }) => {
+  return html`
+    <li class="m-auto align-middle">
+      <a
+        href="#${link}"
+        class="tooltip md:tooltip-left m-1 capitalize font-Rokkit font-bold"
+        data-tip="${title}">
+        ${icon}
+      </a>
+    </li>
+  `
+}).join('')
