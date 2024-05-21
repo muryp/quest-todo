50f84772eea37857eda7b7e78e6278921dc433e7
alifprihantoro <alifprihantoro@gmail.com>
2024-05-21 20:26:03 +0700

    chore: add addons console storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,11 @@
 import type { Preview } from "@storybook/html";
 import '../src/style.css'
+import { setConsoleOptions } from '@storybook/addon-console';
+
+const panelExclude = setConsoleOptions({}).panelExclude;
+setConsoleOptions({
+  panelExclude: [...panelExclude, /deprecated/],
+});
 
 const preview: Preview = {
   parameters: {
