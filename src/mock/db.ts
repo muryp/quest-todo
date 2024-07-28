7e6dd1d25e8ebdb662bb2d066d710c7b6560801d
alifprihantoro <alifprihantoro@gmail.com>
2024-07-28 09:17:11 +0700

    fix: dev mockup

--- src/mock/db.ts
+++ src/mock/db.ts
@@ -1,4 +1,10 @@
-import { add } from '../scripts/db/todo'
+import { add, list } from '../scripts/db/todo'
 import { LIST_QUEST } from './listTodo'
 
-LIST_QUEST.forEach((data) => add(data))
+const DataComplete = await list(5, 0, 'true')
+const DataUnComplete = await list(5, 0, 'false')
+const isHaveData = DataComplete.length || DataUnComplete.length
+if (!isHaveData) {
+  console.log('add data...')
+  LIST_QUEST.forEach((data) => add(data))
+}
