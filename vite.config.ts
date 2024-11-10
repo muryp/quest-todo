4572563e8dba8c506754230f6861b82a86525104
alifprihantoro <alifprihantoro@gmail.com>
2024-11-10 17:23:43 +0700

    chore: fix conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import { ViteMurypJsLiteral } from '@muryp/vite-html'
 
 export default defineConfig({
+  base: 'https://muryp.github.io/quest-todo/',
   plugins: [
     ViteMurypJsLiteral({
       minify: {
